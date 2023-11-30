cbc180016fe843c6c38edb50b686c312b5aea5d2
ashikuraju16 <ashikur.r.raju@gmail.com>
2023-11-30 15:32:11 +0600

    cart

--- js/cart.js
+++ js/cart.js
@@ -2,20 +2,22 @@ document
   .getElementById("input-btn-plus")
   .addEventListener("click", function () {
     const caseNumberField = document.getElementById("input-field");
-    const caseNumberFieldString = caseNumberField.value;
-    const previouseCaseNumber = parseFloat(caseNumberFieldString);
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
 
-    const newNumber = previouseCaseNumber + 1;
+    const newNumber = previousCaseNumber + 1;
+
+    caseNumberField.value = newNumber;
+  });
+  document
+  .getElementById("input-btn-minus")
+  .addEventListener("click", function () {
+    const caseNumberField = document.getElementById("input-field");
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    const newNumber = previousCaseNumber - 1;
+
     caseNumberField.value = newNumber;
   });
 
-  document
- .getElementById("input-btn-minus").addEventListener("click", function (){
-    const caseNumberField = document.getElementById("input-field");
-    const caseNumberFieldString = caseNumberField.value;
-    const previouseCaseNumber = parseFloat(caseNumberFieldString);
-
-    const newNumber = previouseCaseNumber - 1;
-    caseNumberField.value = newNumber;
- 
- })
